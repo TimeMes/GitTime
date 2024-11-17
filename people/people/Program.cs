b37b6eb49ae1b11acf2499026b4dbe8394ad3a89
TimeMes <tiko16tiko16@gmail.com>
2024-11-17 13:03:30 +0400

    Add files via upload

---
+++ people/people/Program.cs
@@ -0,0 +1,202 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+MongoClient client = new MongoClient("mongodb://localhost:27017");
+var database = client.GetDatabase("bigproject");
+IMongoCollection<People> peopleCollection = database.GetCollection<People>("people");
+
+StartPhrase();
+Reader.backToMenu += () => Switcher();
+Switcher();
+
+void StartPhrase()
+{
+    Console.WriteLine("Write one of the list");
+    Console.WriteLine("New people - new \nAll People List - all \nSearch People - search ");
+}
+
+void Switcher()
+{
+    switch (Reader.ReadLine<string>().ToLower())
+    {
+        case "search":
+            Search();
+            Console.WriteLine("Backed to Menu");
+            break;
+        case "search+":
+            SearchPlus();
+            Console.WriteLine("Backed to Menu");
+            break;
+        case "new":
+            NewPeople();
+            Console.WriteLine("Backed to Menu");
+            break;
+        case "delete":
+            PeopleListDelete(new BsonDocument());
+            Console.WriteLine("Backed to Menu");
+            break;
+        case "all":
+            AllPeople();
+            Console.WriteLine("Backed to Menu");
+            break;
+        case "clear" or "":
+            Console.Clear();
+            StartPhrase();
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Its not correct try again");
+            StartPhrase();
+            break;
+    }
+    Switcher();
+}
+
+void SearchPlus()
+{
+    BsonDocument bsons = new BsonDocument();
+    // bsons = ReaderPlus.CompareInt();
+    //var filter = new BsonDocument("Age", bsons);
+    var filt = Builders<People>.Filter.StringIn(human => human.Age.ToString());
+    Console.WriteLine(filt.ToBsonDocument());
+    PeopleListWrite(filt.ToBsonDocument());
+}
+void AllPeople()
+{
+    PeopleListWrite(new BsonDocument());
+}
+
+async void NewPeople()
+{
+    People human = new People();
+    Console.Write("Name ");
+    human.Name = Reader.ReadLine<string>(str => str != "");
+    Console.Write("Age ");
+    human.Age = Reader.ReadLine<int>(age => age > 0 && age < 100);
+    Console.Write("Balance ");
+    human.Balance = Reader.ReadLine<int>();
+    Console.WriteLine($"Save: {human.Name} {human.Age} years old with {human.Balance}$");
+    await peopleCollection.InsertOneAsync(human);
+}
+
+void Search()
+{
+    Console.WriteLine("Search by Name/Age/Balance");
+    string[] keys = { "Name", "Age", "Balance" };
+    string key = Reader.ReadLine<string>(line => keys.Any(key => key == line));
+    Console.Write($"Searching by {key}:");
+    BsonValue value = 0;
+    value = key switch
+    {
+        "Name" => Reader.ReadLine<string>(),
+        "Age" or "Balance" => Reader.ReadLine<int>(i => i > 0),
+        _ => value
+    };
+    var filter = new BsonDocument(key, value);
+    PeopleListWrite(filter);
+}
+
+void PeopleListWrite(BsonDocument filter)
+{
+    var people = PeopleList(filter).Result;
+    if (people.Count == 0)
+    {
+        Console.WriteLine("Sorry, searching end without result");
+        return;
+    }
+    foreach (People p in people)
+    {
+        Console.WriteLine($"{p.Name} is {p.Age} years old and has {p.Balance}$");
+    }
+
+    Console.WriteLine("Press Enter for Continue || E for edit || D for delete");
+    switch (Reader.ReadLine<string>().ToLower())
+    {
+        case "e":
+            PeopleListEdit(filter);
+            break;
+        case "d":
+            PeopleListDelete(filter);
+            break;
+    }
+}
+
+void PeopleListEdit(BsonDocument filter)
+{
+    var people = PeopleList(filter).Result;
+    foreach (People p in people)
+    {
+        Console.WriteLine($"Edit {p.Name} {p.Age} years old {p.Balance}$");
+        Console.WriteLine("Y for yes || X for end editing || Enter for skip");
+        switch (Reader.ReadLine<string>().ToLower())
+        {
+            case "y":
+                Console.WriteLine("Edit Name||Age||Balance");
+                BsonDocument BsonHuman = p.ToBsonDocument();
+                switch (Reader.ReadLine<string>().ToLower())
+                {
+                    case "name":
+                        Console.Write("New name for " + p.Name + ": ");
+                        p.Name = Reader.ReadLine<string>();
+                        break;
+                    case "age":
+                        Console.Write("New age for " + p.Name + ": ");
+                        p.Age = Reader.ReadLine<int>();
+                        break;
+                    case "balance":
+                        Console.Write("New balance for " + p.Name + ": ");
+                        p.Balance = Reader.ReadLine<int>();
+                        break;
+                }
+                peopleCollection.ReplaceOne(BsonHuman, p);
+                break;
+            case "x":
+                return;
+            case "":
+                Console.WriteLine("Skiped");
+                break;
+            default:
+                Console.WriteLine("Unknown option.. skiped");
+                break;
+        }
+    }
+}
+void PeopleListDelete(BsonDocument filter)
+{
+    var people = PeopleList(filter).Result;
+    foreach (People p in people)
+    {
+        Console.WriteLine($"Delete {p.Name} {p.Age} years old {p.Balance}$");
+        Console.WriteLine("Y for yes || X for end deleting || Enter for skip");
+        switch (Reader.ReadLine<string>().ToLower())
+        {
+            case "y":
+                peopleCollection.DeleteOne(p.ToBsonDocument());
+                break;
+            case "x":
+                return;
+            case "":
+                Console.WriteLine("Skiped");
+                break;
+            default:
+                Console.WriteLine("Unknown option.. skiped");
+                break;
+        }
+    }
+}
+async Task<List<People>> PeopleList(BsonDocument filter)
+{
+    var cursor = await peopleCollection.FindAsync(filter);
+    Task<List<People>> peopleTask = Task.Run(() => cursor.ToList());
+    return peopleTask.Result;
+}
+
+[BsonIgnoreExtraElements]
+class People
+{
+    public string Name = "Unknown";
+    public int Age = 0;
+    public int Balance = 0;
+}
+
---
+++ people/people/reader.cs
@@ -0,0 +1,104 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Runtime.InteropServices.JavaScript;
+using static Reader;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+static class Reader
+{
+    public delegate bool Question<T>(T readline);
+    public delegate void Backer();
+    static public Backer backToMenu = () => Console.WriteLine("Backed to Menu");
+    public static T ReadLine<T>()
+    {
+        string? s = Console.ReadLine();
+        if (s is null)
+        {
+            Console.WriteLine("Null Error");
+            return ReadLine<T>();
+        }
+        else if(s.ToLower() == "end")
+        {
+            Environment.Exit(0);
+            return ReadLine<T>();
+        }
+        else if(s.ToLower() == "menu" && backToMenu is not null)
+        {
+            backToMenu();
+            Environment.Exit(0);
+            return default;
+        }
+        else
+        {
+            try
+            {
+                return (T)Convert.ChangeType(s, typeof(T));
+            }
+            catch
+            {
+                Console.WriteLine($"Not {typeof(T)}");
+                return ReadLine<T>();
+            }
+        }
+    }
+
+    public static T ReadLine<T>(Question<T> question)
+    {
+        T t = ReadLine<T>();
+        if (question(t)) return t;
+        else
+        {
+            Console.WriteLine($"Invalid value");
+           
+            return ReadLine<T>(question);
+        }
+    }
+}
+
+static class ReaderPlus
+{
+    private static string Comparer = "$";
+    public static BsonDocument CompareInt()
+    {
+        Comparer = "$";
+        string? s = Console.ReadLine();
+        int Skip = 1;
+        if(s is null || s.Length <  2)
+        {
+            Console.WriteLine("Null Error");
+            return CompareInt();
+        }
+        if (s[0] == '>')
+        {
+            Comparer += "gt";
+        }
+        else if (s[0] == '<')
+        {
+            Comparer += "lt";
+        }
+        else
+        {
+            Console.WriteLine("Invalid Value");
+            return CompareInt();
+        }
+        if (s[1] == '=')
+        {
+            Comparer += "e";
+            Skip = 2;
+        }
+
+        s = string.Join("", s.Skip(Skip));
+
+
+        if (int.TryParse(s, out int result))
+        {
+            return new BsonDocument(Comparer, result);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Value");
+            return CompareInt();
+        }
+    }
+}
